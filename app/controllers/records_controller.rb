6f3312c7a0fc8d722802859f91aa22f6e41923c2
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-03-25 14:16:55 -0400

    adds controllers and more seed data

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,14 @@
 class RecordsController < ApplicationController
+
+  def index
+    @records = Record.all
+
+    render json: @records
+  end
+
+  def show
+    @record = Record.find(params[:id])
+
+    render json: @record
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Record.destroy_all
+# User.destroy_all
+# Collection.destroy_all
+#
+# record1 = Record.create(title: "Toxicity", artist: "System Of A Down", genre: "Rock", image_url: "https://img.discogs.com/aJdQfVMs7J_tjAUuy9vO0UEoppc=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-370466-1536614791-9667.jpeg.jpg")
+# record2 = Record.create(title: "It's Dark And Hell Is Hot", artist: "DMX", genre: "Hip Hop", image_url: "https://img.discogs.com/1yU3Nco56ivgmsSlZS_s97C3MCU=/fit-in/600x599/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3911657-1431034367-1686.png.jpg")
+# record3 = Record.create(title: "Floral Shoppe", artist: "Macintosh Plus", genre: "Vaporwave", image_url: "https://img.discogs.com/ytbgef_OaTUlDg8N3zEOJnHLKUo=/fit-in/600x601/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-9846531-1487279511-4786.png.jpg")
+# user1 = User.create(username: "Farhan85", bio: "I am Farhan!!! Not Faizan", favorite_artist: "Damien Marley", favorite_record: "Stoney Hill", favorite_genre: "Anything")
+# user2 = User.create(username: "Matt22", bio: "I like music", favorite_artist: "18 carrat affair", favorite_record: "Floral Shoppe", favorite_genre: "Vaporwave")
+# user3 = User.create(username: "Bdix92", bio: "I also like music", favorite_artist: "Neil Young", favorite_record: "Abbey Road", favorite_genre: "Country")
+# Collection.create(user_id: user1.id, record_id: record2.id)
+# Collection.create(user_id: user1.id, record_id: record1.id)
+# Collection.create(user_id: user2.id, record_id: record3.id)
+# Collection.create(user_id: user3.id, record_id: record3.id)
