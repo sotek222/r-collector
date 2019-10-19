77f5dcef971f3f76f2fd04b51ae49a74aade9acd
Matthew Masiello <sotek222@yahoo.com>
2019-10-18 20:43:20 -0400

    adds password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_173826) do
+ActiveRecord::Schema.define(version: 2019_10_19_003857) do
 
   create_table "collections", force: :cascade do |t|
     t.integer "user_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_03_25_173826) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
