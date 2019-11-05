49a315fd2c6a53fbfbd99135cf147cff8f1846bc
Matthew Masiello <sotek222@yahoo.com>
2019-11-05 17:52:45 -0500

    removes instance variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
-    render json: @users.to_json(include: [:records])
+    users = User.all
+    render json: users.to_json(include: [:records])
   end
 
   def show
-    @user = User.find(params[:id])
-    render json: @user
+    user = User.find(params[:id])
+    render json: user
   end
   
   def create
-    @user = User.find_or_create_by(email: params[:email])
-    render json: @user
+    user = User.find_or_create_by(email: params[:email])
+    render json: user
   end
 end
